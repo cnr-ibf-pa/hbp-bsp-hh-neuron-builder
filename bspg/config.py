2738d3086b9a2a60e75e27c5812cf87a14fbca2b
Francesco Franchina <cescus92@gmail.com>
2016-11-16 17:45:14 +0100

    Now the project is able to run in local (see following comments)

--- bspg/config.py
+++ bspg/config.py
@@ -1,4 +1,7 @@
-import hbp_app_python_auth.settings as auth_settings
+import os
 
-auth_settings.SOCIAL_AUTH_HBP_KEY = '0643409b-f9e5-4b05-8f46-0b3e98294631'
-auth_settings.SOCIAL_AUTH_HBP_SECRET = 'AKWvSUNdprd7JpVF73RRFlIgxnUMTUzoImWt5i3T-Q39fZRfFsuaCS0_y6DnNcfsNKm8gEBbdt-jWWC42PwMdZw'
+if 'HBPDEV' not in os.environ:
+    import hbp_app_python_auth.settings as auth_settings
+
+    auth_settings.SOCIAL_AUTH_HBP_KEY = '0643409b-f9e5-4b05-8f46-0b3e98294631'
+    auth_settings.SOCIAL_AUTH_HBP_SECRET = 'AKWvSUNdprd7JpVF73RRFlIgxnUMTUzoImWt5i3T-Q39fZRfFsuaCS0_y6DnNcfsNKm8gEBbdt-jWWC42PwMdZw'
