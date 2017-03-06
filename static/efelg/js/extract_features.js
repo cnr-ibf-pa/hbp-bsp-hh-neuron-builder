47cdf458a7869f0500f798b788e60780ee446682
Luca Leonardo Bologna <lbologna@bspg.pa.ibf.cnr.it>
2017-03-06 01:17:16 +0100

    deleted pdf iframe on the last page

--- static/efelg/js/extract_features.js
+++ static/efelg/js/extract_features.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
     $.getJSON('/efelg/protocols_json_path', function(data2){
         document.getElementById('iframe-protocols').setAttribute('src', data2['path']);
     });
-    $.getJSON('/efelg/features_pdf_path', function(data3){
-        document.getElementById('iframe-pdf').setAttribute('src', data3['path']);
-    });
+    //$.getJSON('/efelg/features_pdf_path', function(data3){
+    //    document.getElementById('iframe-pdf').setAttribute('src', data3['path']);
+    //});
     $(".toggle-text").on("click", function(){
         $(this).siblings("div").slideToggle("normal", function(){});
     });
