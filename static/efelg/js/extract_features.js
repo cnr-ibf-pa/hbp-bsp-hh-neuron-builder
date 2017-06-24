53e32454a8e777bff6c068c6038a5559ae74f780
Luca Leonardo Bologna <lbologna@bspg.pa.ibf.cnr.it>
2017-06-24 16:34:15 +0000

    "Running feature extraction /app/media/efel_data/efel_gui/results/lbologna/20170624163354/u_res"

--- static/efelg/js/extract_features.js
+++ static/efelg/js/extract_features.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
 
+    $.getJSON('/efelg/features_json_path', function(data){
 var btn = document.createElement("BUTTON");
-var t = document.createTextNode("CLICK ME");
+var t = document.createTextNode("CLICK MEEEEE");
 btn.appendChild(t);
 document.body.appendChild(btn);
-    $.getJSON('/efelg/features_json_path', function(data){
 
         console.log(data['path'])
             $.get('/hh-neuron-builder/set-feature-folder/' + data['path']);
