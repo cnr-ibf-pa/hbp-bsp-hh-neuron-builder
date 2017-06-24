77d6dc05c878d0d58c8eee6f6a6b230a91afb87d
Luca Leonardo Bologna <lbologna@bspg.pa.ibf.cnr.it>
2017-06-24 15:27:29 +0000

    "Running feature extraction /app/media/efel_data/efel_gui/results/lbologna/20170624152704/u_res"

--- static/hh_neuron_builder/js/set_optimization_parameters.js
+++ static/hh_neuron_builder/js/set_optimization_parameters.js
@@ -1,40 +1,9 @@
 $(document).ready(function(){
     document.getElementById("op-btn").onclick = launchOptimizationHPC;
-
-
-
-
-
-        document.getElementById("back-to-workflow").onclick = workflowPage;
-
-
-
-
-
-
-    /*
-       $.getJSON("/hh-neuron-builder/get_model_list", function(data){
-       $.each(data, function(idx, val){
-       $.each(val, function(index, e){
-       $("#sub-title-div" ).after("<div id=" + index + " class='model-info-div'></div>");
-       $("#" + index).append("<div id=" + index + " class='model-info-div-title'>" + e['meta']['species'] + ' > ' + e['meta']['brain_structure'] + ' > ' + e['meta']['cell_soma_location'] + ' > ' +  e['meta']['cell_type'] + ' > ' + e['meta']['e-type'] + ' > ' + e['meta']['morphology'] +  "</div>");
-       $('#' + index).append("<div><br>Mod files: " + e['meta']['channels'] + "</div>");
-       $('#' + index).append("<div><br>Contributors: " + e['meta']['contributors'] + "</div>");
-       $('#' + index).append("<div><br>Contact: " + e['meta']['email'] + "</div>");
-       })
-       })
-       });
-       });
-
-       $('body').on('click', '.model-info-div', function(){
-       var optimization_name = $(this).attr('id');
-       $.get('/hh-neuron-builder/set-optimization-name/' + optimization_name) 
-       window.location.href = "/hh-neuron-builder/launch-optimization-hpc/";
-       });
-       */
+    document.getElementById("back-to-workflow").onclick = workflowPage;
 });
-function launchOptimizationHPC() {
 
+function launchOptimizationHPC() {
     var gennum = document.getElementById("gen_num").value;
     var offsize = document.getElementById("off_size").value;
     var nodenum = document.getElementById("node_num").value;
