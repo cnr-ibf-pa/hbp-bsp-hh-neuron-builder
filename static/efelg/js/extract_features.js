49fcf3be60808138691a3ddc9ccb06f2f1d0edf8
Luca Leonardo Bologna <lbologna@bspg.pa.ibf.cnr.it>
2017-06-24 16:30:04 +0000

    "Running feature extraction /app/media/efel_data/efel_gui/results/lbologna/20170624162938/u_res"

--- static/efelg/js/extract_features.js
+++ static/efelg/js/extract_features.js
@@ -1,24 +1,13 @@
+var btn = document.createElement("BUTTON");
+var t = document.createTextNode("CLICK ME");
+btn.appendChild(t);
+document.body.appendChild(btn);
+
 $(document).ready(function(){
-    //$.getJSON('/efelg/features_json_path', function(data){
-    //    document.getElementById('iframe-features').setAttribute('src', data['path']);
-    //});
-    //$.getJSON('/efelg/protocols_json_path', function(data2){
-    //    document.getElementById('iframe-protocols').setAttribute('src', data2['path']);
-    //});
-    //$.getJSON('/efelg/features_pdf_path', function(data3){
-    //    document.getElementById('iframe-pdf').setAttribute('src', data3['path']);
-    //});
-    //
+
     $.getJSON('/efelg/features_json_path', function(data){
-        
+
         console.log(data['path'])
-        $.get('/hh-neuron-builder/set-feature-folder/' + data['path']);
+            $.get('/hh-neuron-builder/set-feature-folder/' + data['path']);
     });
-                var btn = document.createElement("BUTTON");
-            var t = document.createTextNode("CLICK ME");
-                btn.appendChild(t);
-                    document.body.appendChild(btn);
-    //$(".toggle-text").on("click", function(){
-    //    $(this).siblings("div").slideToggle("normal", function(){});
-    //});
 });
