6fbc534c5ee68fe9f3677af1cf31709c0dfa7ed2
Luca Leonardo Bologna <lbologna@bspg.pa.ibf.cnr.it>
2017-06-25 16:27:19 +0000

    "Running feature extraction /app/media/efel_data/efel_gui/results/lbologna/20170625162647/u_res"

--- static/hh_neuron_builder/js/embedded_efel_gui.js
+++ static/hh_neuron_builder/js/embedded_efel_gui.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
     document.getElementById("back-to-workflow").onclick = workflowPage;
-    document.getElementById("save-feat-files").onclick = saveFeatureFiles;
 });
 
 function workflowPage() {
-    window.location.href = "/hh-neuron-builder/workflow/";
-}
-
-function saveFeatureFiles() {
+    console.log()
     window.location.href = "/hh-neuron-builder/workflow/";
 }
