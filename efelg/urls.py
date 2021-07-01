a4614a6f09566382d6ba06da4c18408f353c5910
rcsm17 <robertosmiriglia@gmail.com>
2021-07-01 17:42:19 +0200

    added EfelStorage class to serve static path instead of using session

--- efelg/urls.py
+++ efelg/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
-
+from django.urls import path, register_converter
+from hhnb.utils.converters import WorkflowIdConverter
 from efelg import views
+
+
+register_converter(WorkflowIdConverter, 'workflow_id')
 
 
 urlpatterns = [
     path('', views.overview),
-    path('overview/', views.overview),
+    path('overview/<', views.overview),
     path('get_list', views.get_list),
     path('get_data/<slug:cellname>', views.get_data),
     path('show_traces/', views.show_traces),
@@ -20,20 +23,6 @@ urlpatterns = [
     path('docs/file_formats/', views.file_formats_docs),
 
     path('get_result_dir/', views.get_result_dir),
-
-    # url(r'^login/hbp', auth_views.login),
-    # url(r'^logout/hbp', auth_views.logout),
-    # path('generate_json_data', views.generate_json_data),
-    # path('get_list_new', views.get_list_new),
-    # path('get_data/(?P<cellname>[0-9a-zA-Z_-]+)', views.get_data),
-    # path('status/', views.status),
-    # path('features_json_path', views.features_json_path),
-    # path('features_json_files_path', views.features_json_files_path),
-    # path('protocols_json_path', views.protocols_json_path),
-    # path('features_pdf_path', views.features_pdf_path),
-    # path('get_directory_structure', views.get_directory_structure), 
-    # path('upload_zip_file_to_storage', views.upload_zip_file_to_storage),
-    # path('hbp-redirect/', views.hbp_redirect),
     
     path('hhf_etraces/', views.hhf_etraces),
     path('load_hhf_etraces/', views.load_hhf_etraces),
