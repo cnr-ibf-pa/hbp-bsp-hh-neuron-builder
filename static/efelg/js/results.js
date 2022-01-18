e34ed0ae4f2ed44c22c126093129067f77534ff5
Luca L. Bologna <lucaleonardo.bologna@gmail.com>
2022-01-18 16:14:57 +0100

    handled error during feature extraction: inserted error message when it fails

--- static/efelg/js/results.js
+++ static/efelg/js/results.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
     window.scrollTo(0,0);
     openMessageDiv("wait-div", "main-e-res-div");
     $.getJSON('/efelg/extract_features', function(data){
-        if (data["status"] == "OK"){
-            $.getJSON('/efelg/get_result_dir/', function(data) {
+        if (data["status"] == "OK") {
+            $.getJSON('/efelg/get_result_dir/', function (data) {
                 document.getElementById("hiddendiv").className = JSON.parse(data).result_dir
             });
             closeMessageDiv("wait-div", "main-e-res-div");
-        };
+        } else {
+            closeMessageDiv("wait-div", "main-e-res-div");
+            openMessageDiv("e-res-error-div", "main-e-res-div");
+            writeMessage("error-message-div", "An error occurred while extracting features from traces. " + data["message"])
+        }
     });
 });
